d35a95e1d9361d77e6c60ebf937534ebeece6ba5
jieyupan <jieyu-pan@uiowa.edu>
2015-02-17 20:09:30 -0600

    aspx. edited

--- Default.aspx
+++ Default.aspx
@@ -5,30 +5,31 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
     <title>Salary Calculator</title>
+    <link rel="stylesheet" type="text/css" href="stylesheet.css" />
 </head>
 <body>
     <form id="form1" runat="server">
     <div>
     
         <h1>Salary Calculation</h1><br />
-        <br />
+        
        <p>Hourly wage:<asp:TextBox ID="tb_hourlywage" runat="server"></asp:TextBox></P>
         <br />
-        <br />
+
        <p>Number of hours worked:<asp:TextBox ID="tb_hoursworked" runat="server"></asp:TextBox></p>
         <br />
-        <br />
+
        <p>Pre-tax deductions:<asp:TextBox ID="tb_pretaxdeductions" runat="server"></asp:TextBox></p>
         <br />
-        <br />
+
        <p>After-tax deductions:<asp:TextBox ID="tb_aftertaxdeduction" runat="server"></asp:TextBox></p>
         <br />
-        <br />
+ 
         <p><asp:Button ID="bt_calculate" runat="server" Text="Calculate Net Pay" />
 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
         <asp:Button ID="bt_clear" runat="server" Text="Clear" /></p>
         <br />
-        <br />
+ 
         <p>Net pay: <asp:Label ID="lbl_netpay" runat="server"></asp:Label></p>
     
     </div>
--- Default.aspx.vb
+++ Default.aspx.vb
@@ -10,30 +10,30 @@ Partial Class _Default
         Const tax_hat As Integer = 500
 
         'Create variables to hold inputs
-        Dim houlywage As Decimal = tb_hourlywage.Text
-        Dim hoursworked As Decimal = tb_hoursworked.Text
+        Dim wage As Decimal = tb_hourlywage.Text
+        Dim hours As Decimal = tb_hoursworked.Text
         Dim pre_tax_deduction As Decimal = tb_pretaxdeductions.Text
         Dim after_tax_deduction As Decimal = tb_aftertaxdeduction.Text
 
         'income after pre-tax-deduction
-        Dim income_after_pre As Decimal
+        Dim taxable_pay As Decimal
         Dim tax_amount As Decimal
-        income_after_pre = houlywage * hoursworked - pre_tax_deduction
+        taxable_pay = wage * hours - pre_tax_deduction
 
         'calculate tax amount
-        If income_after_pre < tax_hat Then
-            tax_amount = income_after_pre * tax_rate_small
+        If taxable_pay < tax_hat Then
+            tax_amount = taxable_pay * tax_rate_small
         Else
-            tax_amount = income_after_pre * tax_rate_big
+            tax_amount = taxable_pay * tax_rate_big
         End If
 
         'calculate after tax income
-        Dim income_after_tax As Decimal
-        income_after_tax = income_after_pre - tax_amount
+        Dim pay As Decimal
+        pay = taxable_pay - tax_amount
 
         'calculate net pay
         Dim net_pay As Decimal
-        net_pay = income_after_tax - after_tax_deduction
+        net_pay = pay - after_tax_deduction
 
         'display net pay
         lbl_netpay.Text = String.Format("{0:C}", net_pay)
